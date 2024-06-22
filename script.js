114594ee89517accf92d8e43f3791a61297442ff
BrMaro <70138725+BrMaro@users.noreply.github.com>
2024-06-22 20:23:39 +0300

    styling

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 let price = 19.5;
 let cid = [
-  ["PENNY", 1.01],
-  ["NICKEL", 2.05],
-  ["DIME", 3.1],
-  ["QUARTER", 4.25],
-  ["ONE", 90],
-  ["FIVE", 55],
-  ["TEN", 20],
-  ["TWENTY", 60],
-  ["ONE HUNDRED", 100]
+    ["PENNY", 1.01],
+    ["NICKEL", 2.05],
+    ["DIME", 3.1],
+    ["QUARTER", 4.25],
+    ["ONE", 90],
+    ["FIVE", 55],
+    ["TEN", 20],
+    ["TWENTY", 60],
+    ["ONE HUNDRED", 100]
 ];
 const changeDue = document.getElementById("change-due");
 const purchaseButton = document.getElementById("purchase-btn");
